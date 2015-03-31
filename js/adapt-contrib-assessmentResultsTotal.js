b2117f5a97433ad063b8884aebf61303c952fe73
Oliver Foster <oliver.foster@kineo.com>
2015-03-31 15:56:52 +0100

    Initial commit

---
+++ js/adapt-contrib-assessmentResultsTotal.js
@@ -0,0 +1,138 @@
+/*
+* adapt-contrib-assessmentResultsTotal
+* License - http://github.com/adaptlearning/adapt_framework/LICENSE
+* Maintainers - Daryl Hedley <darylhedley@hotmail.com>, "Oliver Foster <oliver.foster@kineo.com>"
+*/
+define(function(require) {
+
+	var ComponentView = require('coreViews/componentView');
+	var Adapt = require('coreJS/adapt');
+
+    var AssessmentResultsTotal = ComponentView.extend({
+
+        events: {
+            'inview .component-body': 'onInview'
+        },
+
+        preRender: function () {
+            this.setupEventListeners();
+
+            this.model.set('_isVisible', false);
+        },
+
+        postRender: function() {
+            this.setReadyStatus();
+        },
+        setupEventListeners: function() {
+            this.listenTo(Adapt, 'assessment:complete', this.onAssessmentComplete);
+
+            this.listenToOnce(Adapt, 'remove', this.onRemove);
+        },
+
+        removeEventListeners: function() {;
+            this.stopListening(Adapt, 'assessment:complete', this.onAssessmentComplete);
+
+            this.stopListening(Adapt, 'remove', this.onRemove);
+        },
+
+        onAssessmentComplete: function(state) {
+            this.model.set("_state", state);
+            this.setFeedback();
+
+             //show feedback component
+            if(!this.model.get('_isVisible')) this.model.set('_isVisible', true, {pluginName: '_results'});
+            this.render();
+        },
+
+        onInview: function(event, visible, visiblePartX, visiblePartY) {
+            if (visible) {
+                if (visiblePartY === 'top') {
+                    this._isVisibleTop = true;
+                } else if (visiblePartY === 'bottom') {
+                    this._isVisibleBottom = true;
+                } else {
+                    this._isVisibleTop = true;
+                    this._isVisibleBottom = true;
+                }
+                
+                if (this._isVisibleTop || this._isVisibleBottom) {
+                    this.setCompletionStatus();
+                }
+            }
+        },
+
+        setFeedback: function() {
+
+            var completionMessage = this.model.get("_completionMessage").message;
+            var feedbackBand = this.getFeedbackBand();
+
+            var state = this.model.get("_state");
+            state.feedbackBand = feedbackBand;
+            state.feedback = feedbackBand.feedback;
+
+            completionMessage = this.stringReplace(completionMessage, state);
+
+            this.model.set("completionMessage", completionMessage);
+
+        },
+
+        getFeedbackBand: function() {
+            var state = this.model.get("_state");
+
+            var bands = this.model.get("_bands");
+            var scoreAsPercent = state.scoreAsPercent;
+            
+            for (var i = (bands.length - 1); i >= 0; i--) {
+                if (scoreAsPercent >= bands[i]._score) {
+                    return bands[i];
+                }
+            }
+
+            return "";
+        },
+
+        stringReplace: function(string, context) {
+            //use handlebars style escaping for string replacement
+            //only supports unescaped {{{ attributeName }}} and html escaped {{ attributeName }}
+            //will string replace recursively until no changes have occured
+
+            var changed = true;
+            while (changed) {
+                changed = false;
+                for (var k in context) {
+                    var contextValue = context[k];
+
+                    switch (typeof contextValue) {
+                    case "object":
+                        continue;
+                    case "number":
+                        contextValue = Math.floor(contextValue);
+                        break;
+                    }
+
+                    var regExNoEscaping = new RegExp("((\\{\\{\\{){1}[\\ ]*"+k+"[\\ ]*(\\}\\}\\}){1})","g");
+                    var regExEscaped = new RegExp("((\\{\\{){1}[\\ ]*"+k+"[\\ ]*(\\}\\}){1})","g");
+
+                    var preString = string;
+
+                    string = string.replace(regExNoEscaping, contextValue);
+                    var escapedText = $("<p>").text(contextValue).html();
+                    string = string.replace(regExEscaped, escapedText);
+
+                    if (string != preString) changed = true;
+
+                }
+            }
+
+            return string;
+        },
+
+        onRemove: function() {
+            this.removeEventListeners();
+        }
+        
+    });
+    
+    Adapt.register("assessmentResultsTotal", AssessmentResultsTotal);
+    
+});
